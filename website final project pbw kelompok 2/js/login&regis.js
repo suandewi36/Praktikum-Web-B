878e54019ea45be8d0d2484a86b1310aaed8a671
suandewi <71535646+suandewi36@users.noreply.github.com>
2020-11-04 22:09:33 +0800

    uts progress final projeck pbw kelompok2

---
+++ website final project pbw kelompok 2/js/login&regis.js
@@ -0,0 +1,14 @@
+var a = document.getElementById("login");
+		var b = document.getElementById("register");
+		var c = document.getElementById("btn");
+
+		function register(){
+			a.style.left = "-400px";
+			b.style.left = "50px";
+			c.style.left = "110px";
+		}
+		function login(){
+			a.style.left = "50px";
+			b.style.left = "450px";
+			c.style.left = "0px";
+		}
